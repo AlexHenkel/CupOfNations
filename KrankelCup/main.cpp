1f74da016c5c6e571777c1d7287391ab8cf05f47
Alex Henkel <alehenkel17@gmail.com>
2015-03-08 00:55:06 -0600

    Initial Commit

---
+++ KrankelCup/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  KrankelCup
+//
+//  Created by Alejandro Henkel on 3/8/15.
+//  Copyright (c) 2015 Alejandro Henkel. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
