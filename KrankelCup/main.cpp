9bbe97428f8edea7b04c67ab6406e63ca8f9825f
Alejandro Henkel <alehenkel17@gmail.com>
2015-03-10 13:41:39 -0600

    Agrega tabla de equipos

--- KrankelCup/main.cpp
+++ KrankelCup/main.cpp
@@ -60,6 +60,7 @@ public:
     void sumarGolesC(int iGolesC){this->iGolesC += iGolesC; iDif = iGolesF - this->iGolesC;};
     void sumarPartido(){iPJ++; dPorcentaje = iPuntos / iPJ;};
     void resetPuntos(){iPuntos = 0; iGolesF = 0; iGolesC = 0; iDif = 0;};
+    void resetJugador(){sNombre = ""; iPuntos = 0; iGolesF = 0; iGolesC = 0; iDif = 0; iPJ = 0; dPorcentaje = 0.0;};
 private:
     string sNombre;
     int iPuntos, iGolesF, iGolesC, iDif, iPJ;
@@ -71,6 +72,7 @@ vector<Jugador> jugJugadoresF;
 vector<Jugador> jugJugadoresTabla;
 vector<Partido> parPartidos;
 vector<Partido> parPartidosF;
+bool bDosGrupos = false;
 
 // random generator function:
 int myrandom (int i) { return std::rand()%i;}
@@ -120,17 +122,128 @@ void ordenaGrupos(int iN){
                     jugJugadores[i] = jugJugadores[j];
                     jugJugadores[j] = jugTemp;
                 }
+                else if (jugJugadores[j].getPuntos() == jugJugadores[i].getPuntos()) {
+                    if (jugJugadores[j].getDif() > jugJugadores[i].getDif()) {
+                        jugTemp = jugJugadores[i];
+                        jugJugadores[i] = jugJugadores[j];
+                        jugJugadores[j] = jugTemp;
+                    }
+                    else if (jugJugadores[j].getDif() == jugJugadores[i].getDif()) {
+                        if (jugJugadores[j].getGolesF() > jugJugadores[i].getGolesF()) {
+                            jugTemp = jugJugadores[i];
+                            jugJugadores[i] = jugJugadores[j];
+                            jugJugadores[j] = jugTemp;
+                        }
+                        else if (jugJugadores[j].getGolesF() == jugJugadores[i].getGolesF()) {
+                            if (jugJugadores[j].getGolesC() < jugJugadores[i].getGolesC()) {
+                                jugTemp = jugJugadores[i];
+                                jugJugadores[i] = jugJugadores[j];
+                                jugJugadores[j] = jugTemp;
+                            }
+                        }
+                    }
+                }
             }
         }
     }
     
     for (int i = iN / 2; i < iN ; i++) {
-        for (int j = iN / 2; j < iN; j++) {
+        for (int j = i + 1; j < iN; j++) {
             if (i != j) {
                 if (jugJugadores[j].getPuntos() > jugJugadores[i].getPuntos()) {
                     jugTemp = jugJugadores[i];
                     jugJugadores[i] = jugJugadores[j];
                     jugJugadores[j] = jugTemp;
+                }
+                else if (jugJugadores[j].getPuntos() == jugJugadores[i].getPuntos()) {
+                    if (jugJugadores[j].getDif() > jugJugadores[i].getDif()) {
+                        jugTemp = jugJugadores[i];
+                        jugJugadores[i] = jugJugadores[j];
+                        jugJugadores[j] = jugTemp;
+                    }
+                    else if (jugJugadores[j].getDif() == jugJugadores[i].getDif()) {
+                        if (jugJugadores[j].getGolesF() > jugJugadores[i].getGolesF()) {
+                            jugTemp = jugJugadores[i];
+                            jugJugadores[i] = jugJugadores[j];
+                            jugJugadores[j] = jugTemp;
+                        }
+                        else if (jugJugadores[j].getGolesF() == jugJugadores[i].getGolesF()) {
+                            if (jugJugadores[j].getGolesC() < jugJugadores[i].getGolesC()) {
+                                jugTemp = jugJugadores[i];
+                                jugJugadores[i] = jugJugadores[j];
+                                jugJugadores[j] = jugTemp;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+void ordenaGruposTabla(int iN){
+    Jugador jugTemp;
+    for (int i = 0; i < iN / 2; i++) {
+        for (int j = i + 1; j < iN / 2; j++) {
+            if (i != j) {
+                if (jugJugadoresTabla[j].getPuntos() > jugJugadoresTabla[i].getPuntos()) {
+                    jugTemp = jugJugadoresTabla[i];
+                    jugJugadoresTabla[i] = jugJugadoresTabla[j];
+                    jugJugadoresTabla[j] = jugTemp;
+                }
+                else if (jugJugadoresTabla[j].getPuntos() == jugJugadoresTabla[i].getPuntos()) {
+                    if (jugJugadoresTabla[j].getDif() > jugJugadoresTabla[i].getDif()) {
+                        jugTemp = jugJugadoresTabla[i];
+                        jugJugadoresTabla[i] = jugJugadoresTabla[j];
+                        jugJugadoresTabla[j] = jugTemp;
+                    }
+                    else if (jugJugadoresTabla[j].getDif() == jugJugadoresTabla[i].getDif()) {
+                        if (jugJugadoresTabla[j].getGolesF() > jugJugadoresTabla[i].getGolesF()) {
+                            jugTemp = jugJugadoresTabla[i];
+                            jugJugadoresTabla[i] = jugJugadoresTabla[j];
+                            jugJugadoresTabla[j] = jugTemp;
+                        }
+                        else if (jugJugadoresTabla[j].getGolesF() == jugJugadoresTabla[i].getGolesF()) {
+                            if (jugJugadoresTabla[j].getGolesC() < jugJugadoresTabla[i].getGolesC()) {
+                                jugTemp = jugJugadoresTabla[i];
+                                jugJugadoresTabla[i] = jugJugadoresTabla[j];
+                                jugJugadoresTabla[j] = jugTemp;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    for (int i = iN / 2; i < iN ; i++) {
+        for (int j = i + 1; j < iN; j++) {
+            if (i != j) {
+                if (jugJugadoresTabla[j].getPuntos() > jugJugadoresTabla[i].getPuntos()) {
+                    jugTemp = jugJugadoresTabla[i];
+                    jugJugadoresTabla[i] = jugJugadoresTabla[j];
+                    jugJugadoresTabla[j] = jugTemp;
+                }
+                else if (jugJugadoresTabla[j].getPuntos() == jugJugadoresTabla[i].getPuntos()) {
+                    if (jugJugadoresTabla[j].getDif() > jugJugadoresTabla[i].getDif()) {
+                        jugTemp = jugJugadoresTabla[i];
+                        jugJugadoresTabla[i] = jugJugadoresTabla[j];
+                        jugJugadoresTabla[j] = jugTemp;
+                    }
+                    else if (jugJugadoresTabla[j].getDif() == jugJugadoresTabla[i].getDif()) {
+                        if (jugJugadoresTabla[j].getGolesF() > jugJugadoresTabla[i].getGolesF()) {
+                            jugTemp = jugJugadoresTabla[i];
+                            jugJugadoresTabla[i] = jugJugadoresTabla[j];
+                            jugJugadoresTabla[j] = jugTemp;
+                        }
+                        else if (jugJugadoresTabla[j].getGolesF() == jugJugadoresTabla[i].getGolesF()) {
+                            if (jugJugadoresTabla[j].getGolesC() < jugJugadoresTabla[i].getGolesC()) {
+                                jugTemp = jugJugadoresTabla[i];
+                                jugJugadoresTabla[i] = jugJugadoresTabla[j];
+                                jugJugadoresTabla[j] = jugTemp;
+                            }
+                        }
+                    }
                 }
             }
         }
@@ -163,6 +276,22 @@ void muestraGrupos(int iN){
     }
 }
 
+void muestraTabla(int iN){
+    
+    ordenaGrupos(iN);
+    cout << "Liga 2015" << endl;
+    cout << setw(10) << left << "Equipos";
+    cout << "GF GC Dif Pts %" << endl;
+    for (int k = 0; k < jugJugadoresTabla.size(); k++) {
+        cout << setw(10) << left << jugJugadoresTabla[k].getNombre() << " ";
+        cout << setw(2) << jugJugadoresTabla[k].getGolesF() << " ";
+        cout << setw(2) << jugJugadoresTabla[k].getGolesC() << " ";
+        cout << setw(3) << jugJugadoresTabla[k].getDif() << " ";
+        cout << setw(3) << jugJugadoresTabla[k].getPuntos() << " ";
+        printf("%.3d\n", jugJugadoresTabla[k].getPorcentaje());
+    }
+}
+
 void muestraPartidos() {
     for (int i = 0; i < parPartidos.size(); i++)
         cout << "P" << i + 1 << ": " << parPartidos[i].getLocal() << " vs " << parPartidos[i].getVisitante() << endl;
@@ -177,6 +306,7 @@ void ingresaMarcadorF() {
     int iLocal, iVisitante, iLClave = 0, iVClave = 0, iClave, iClave2;
     bool bExiste = false;
     string sLocal, sVisitante;
+    string sAux;
     
     do {
         cout << "Ingresa nombre del jugador local" << endl;
@@ -205,11 +335,49 @@ void ingresaMarcadorF() {
     iClave = ((iLClave * 10) + iVClave);
     iClave2 = ((iVClave * 10) + iLClave);
     
-    cout << "Ingresa goles del local" << endl;
-    cin >> iLocal;
+    do {
+        cout << "Ingresa goles del local" << endl;
+        cin >> sAux;
+        if (sAux[0] >= 48 && sAux[0] <= 57) {
+            if (sAux.length() == 1) {
+                iLocal = sAux[0] - 48;
+            }
+            else if (sAux.length() == 2) {
+                iLocal = (sAux[0] - 48) * 10;
+                iLocal += (sAux[1] - 48);
+            }
+            else {
+                cout << "Ingresa un número correcto" << endl;
+                iLocal = 10000;
+            }
+        }
+        else {
+            cout << "Ingresa un número correcto" << endl;
+            iLocal = 10000;
+        }
+    } while (iLocal > 0 && iLocal < 16);
     
-    cout << "Ingresa goles del visitante" << endl;
-    cin >> iVisitante;
+    do {
+        cout << "Ingresa goles del visitante" << endl;
+        cin >> sAux;
+        if (sAux[0] >= 48 && sAux[0] <= 57) {
+            if (sAux.length() == 1) {
+                iVisitante = sAux[0] - 48;
+            }
+            else if (sAux.length() == 2) {
+                iVisitante = (sAux[0] - 48) * 10;
+                iVisitante += (sAux[1] - 48);
+            }
+            else {
+                cout << "Ingresa un número correcto" << endl;
+                iVisitante = 10000;
+            }
+        }
+        else {
+            cout << "Ingresa un número correcto" << endl;
+            iVisitante = 10000;
+        }
+    } while (iVisitante > 0 && iVisitante < 16);
     
     jugJugadoresF[iLClave].sumarGolesF(iLocal);
     jugJugadoresF[iLClave].sumarGolesC(iVisitante);
@@ -286,10 +454,45 @@ void ingresaMarcadorF() {
     }
 }
 
+void escribeArchivoTabla() {
+    ofstream ofTabla;
+    ifstream ifTabla;
+    Jugador jugAux;
+    int iAux;
+    string sAux;
+    
+    ofTabla.open("tabla.txt");
+    if (ofTabla.is_open())
+    {
+        for (int i = 0; i < jugJugadoresTabla.size(); i++) {
+            ofTabla << jugJugadoresTabla[i].getNombre() << " " << jugJugadoresTabla[i].getPJ() << " " << jugJugadoresTabla[i].getGolesF() << " " << jugJugadoresTabla[i].getGolesC() << " " << jugJugadoresTabla[i].getPuntos();
+        }
+        ofTabla.close();
+    }
+    else cout << "Unable to open file";
+    jugJugadoresTabla.clear();
+    
+    ifTabla.open("tabla.txt");
+    while (ifTabla >> sAux) {
+        jugAux.setNombre(sAux);
+        ifTabla >> iAux;
+        jugAux.setPJ(iAux);
+        ifTabla >> iAux;
+        jugAux.setGolesF(iAux);
+        ifTabla >> iAux;
+        jugAux.setGolesC(iAux);
+        ifTabla >> iAux;
+        jugAux.setPuntos(iAux);
+        jugJugadoresTabla.push_back(jugAux);
+    }
+    ifTabla.close();
+    ordenaGruposTabla(jugJugadoresTabla.size());
+}
+
 void ingresaMarcador() {
     int iLocal, iVisitante, iLClave = 0, iVClave = 0, iLClaveT = 0, iVClaveT = 0, iClave, iClave2;
     bool bExiste = false, bExisteTL = false, bExisteTV = false;
-    string sLocal, sVisitante;
+    string sLocal, sVisitante, sAux;
     
     do {
         cout << "Ingresa nombre del jugador local" << endl;
@@ -330,11 +533,49 @@ void ingresaMarcador() {
     iClave = ((iLClave * 10) + iVClave);
     iClave2 = ((iVClave * 10) + iLClave);
     
-    cout << "Ingresa goles del local" << endl;
-    cin >> iLocal;
+    do {
+        cout << "Ingresa goles del local" << endl;
+        cin >> sAux;
+        if (sAux[0] >= 48 && sAux[0] <= 57) {
+            if (sAux.length() == 1) {
+                iLocal = sAux[0] - 48;
+            }
+            else if (sAux.length() == 2) {
+                iLocal = (sAux[0] - 48) * 10;
+                iLocal += (sAux[1] - 48);
+            }
+            else {
+                cout << "Ingresa un número correcto" << endl;
+                iLocal = 10000;
+            }
+        }
+        else {
+            cout << "Ingresa un número correcto" << endl;
+            iLocal = 10000;
+        }
+    } while (iLocal > 0 && iLocal < 16);
     
-    cout << "Ingresa goles del visitante" << endl;
-    cin >> iVisitante;
+    do {
+        cout << "Ingresa goles del visitante" << endl;
+        cin >> sAux;
+        if (sAux[0] >= 48 && sAux[0] <= 57) {
+            if (sAux.length() == 1) {
+                iVisitante = sAux[0] - 48;
+            }
+            else if (sAux.length() == 2) {
+                iVisitante = (sAux[0] - 48) * 10;
+                iVisitante += (sAux[1] - 48);
+            }
+            else {
+                cout << "Ingresa un número correcto" << endl;
+                iVisitante = 10000;
+            }
+        }
+        else {
+            cout << "Ingresa un número correcto" << endl;
+            iVisitante = 10000;
+        }
+    } while (iVisitante > 0 && iVisitante < 16);
     
     jugJugadores[iLClave].sumarGolesF(iLocal);
     jugJugadores[iLClave].sumarGolesC(iVisitante);
@@ -430,6 +671,7 @@ void ingresaMarcador() {
             }
         }
     }
+    escribeArchivoTabla();
 }
 
 void final() {
@@ -934,6 +1176,7 @@ void finales(int iPartidosG) {
     int iOpcion, iPartidos = 0;
     Partido parAux;
     string sAux;
+    char cOpcion;
     
     random_shuffle(jugJugadoresF.begin(), jugJugadoresF.end());
     // Partido 1
@@ -963,7 +1206,8 @@ void finales(int iPartidosG) {
         cout << "Selecciona la opción deseada:" << endl;
         cout << "1. Mostrar partidos" << endl;
         cout << "2. Ingresar marcador" << endl;
-        cin >> iOpcion;
+        cin >> cOpcion;
+        iOpcion = cOpcion - 48;
         switch (iOpcion) {
             case 1:
                 muestraPartidosF();
@@ -984,6 +1228,7 @@ void finales(int iPartidosG) {
 
 void grupos(int iN){
     int iOpcion, iPartidos = 0;
+    char cOpcion;
     do {
         cout << "Selecciona la opción deseada:" << endl;
         cout << "1. Mostrar grupos" << endl;
@@ -992,7 +1237,8 @@ void grupos(int iN){
         if (iPartidos >= parPartidos.size()) {
             cout << "4. Pasar a la siguiente ronda" << endl;
         }
-        cin >> iOpcion;
+        cin >> cOpcion;
+        iOpcion = cOpcion - 48;
         switch (iOpcion) {
             case 1:
                 muestraGrupos(iN);
@@ -1010,8 +1256,9 @@ void grupos(int iN){
                 }
                 break;
             case 45:
-                cout << "Seguro quieres pasar a la siguiente ronda? 4 Si, cualquier número para No" << endl;
-                cin >> iOpcion;
+                cout << "Seguro quieres pasar a la siguiente ronda? 4 Si, cualquier tecla para No" << endl;
+                cin >> cOpcion;
+                iOpcion = cOpcion - 48;
                 if (iOpcion == 4) {
                     generaFinalistas(iN);
                 }
@@ -1024,9 +1271,11 @@ void grupos(int iN){
 
 int main() {
     string sAux;
-    int iN = 4, iOpcion, iAux;
+    int iN = 0, iOpcion, iAux;
+    char cOpcion;
     ifstream ifTabla;
     Jugador jugAux;
+    bool bExiste;
     
     ifTabla.open("tabla.txt");
     while (ifTabla >> sAux) {
@@ -1045,20 +1294,64 @@ int main() {
     
     srand (time(NULL));
     
-    cout << "Ingresa número de jugadores: " << endl;
-    cin >> iN;
+    do {
+        cout << "Presiona 1 para agregar jugadores al Torneo de Liga 2015 o cualquier otra tecla para continuar al torneo de hoy" << endl;
+        cin >> cOpcion;
+        iOpcion = cOpcion - 48;
+        if (iOpcion == 1) {
+            jugAux.resetJugador();
+            bExiste = false;
+            cout << "Ingresa el nombre del jugador" << endl;
+            cin >> sAux;
+            jugAux.setNombre(sAux);
+            for (int i = 0; i < jugJugadoresTabla.size() && !bExiste; i++) {
+                if (sAux == jugJugadoresTabla[i].getNombre())
+                    bExiste = true;
+            }
+            if (bExiste) {
+                cout << sAux << " ya está participando en la Liga 2015" << endl;
+            }
+            else {
+                jugJugadoresTabla.push_back(jugAux);
+            }
+        }
+    } while (iOpcion == 1);
+    
+    do {
+        cout << "Ingresa número de jugadores para el torneo de hoy (De 0 a 15): " << endl;
+        cin >> sAux;
+        if (sAux[0] >= 48 && sAux[0] <= 57) {
+            if (sAux.length() == 1) {
+                iN = sAux[0] - 48;
+            }
+            else if (sAux.length() == 2) {
+                iN = (sAux[0] - 48) * 10;
+                iN += (sAux[1] - 48);
+            }
+            else {
+                cout << "Ingresa un número correcto" << endl;
+                iN = 10000;
+            }
+        }
+        else {
+            cout << "Ingresa un número correcto" << endl;
+            iN = 10000;
+        }
+    } while (iN > 0 && iN < 16);
+    
     ingresaJugadores(iN);
     
     do {
-        cout << "Presiona 1 para grupos o 2 para finales (solo para 4 personas) " << endl;
-        cin >> iOpcion;
+        cout << "Presiona 1 para dos grupos o 2 para un grupo" << endl;
+        cin >> cOpcion;
+        iOpcion = cOpcion - 48;
     } while (iOpcion != 1 && iOpcion != 2);
     
-    if (iOpcion == 1) {
-        grupos(iN);
+    if (iOpcion == 2) {
+        bDosGrupos = true;
     }
-    else if (iOpcion == 2)
-        finales(0);
+    grupos(iN);
+    
     
     
     return 0;
